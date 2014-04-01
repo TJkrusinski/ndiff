3ccae9abfd6e8521d5f13c4716e2745af60b7054
TJ Krusinski <tjkrus@gmail.com>
2014-04-01 00:01:29 -0400

    formatting

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ var child_process = require('child_process');
 var concat = require('concat-stream');
 
 /**
- *	Diff two files
+ *  Diff two files
  */
 
 module.exports = function(file1, file2, cb) {
-	var process = child_process.spawn('diff', [file1, file2, '-u']);
+    var process = child_process.spawn('diff', [file1, file2, '-u']);
     if (cb) return process.stdout.pipe(concat({encoding: 'string'}, cb));
     return process.stdout;
 };
